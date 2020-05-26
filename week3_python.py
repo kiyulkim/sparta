506ca62638c73d33ae23147649b8ed6d7b79f187
kiyulkim <64315663+kiyulkim@users.noreply.github.com>
2020-05-26 21:39:28 +0900

    파이썬드디어시작이다

---
+++ week3_python.py
@@ -0,0 +1,20 @@
+print('hello')
+name = 'bob' #111
+# let 없고 ; 없고, 주석은 #
+num = 12
+is_number = True #js 에서는 true(대소문자 주의)
+
+a_list=[] #  빈리스트
+a_list.append(1) #js 에서는 push
+a_list[0] #호출은 동일
+print(a_list) # console.log
+
+a_dict = {}
+a_dict = {'name': 'bob', 'age': 21}
+a_dict['height'] = 178 #새로추가
+print(a_dict['name']) # 값 가져오기 동일함
+
+print(a_dict)
+
+people =[{'name':'bob','age':20},{'name':'carry','age':25}]
+print(people[0]['name'])
