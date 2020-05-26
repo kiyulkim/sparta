f9137da15d2ee975302f058c304dbd8e8b1cd7dc
kiyulkim <64315663+kiyulkim@users.noreply.github.com>
2020-05-26 22:09:03 +0900

    밥이랑 캐리랑 나이를 알았다

--- week3_python.py
+++ week3_python.py
@@ -18,3 +18,64 @@ print(a_dict)
 
 people =[{'name':'bob','age':20},{'name':'carry','age':25}]
 print(people[0]['name'])
+
+
+# function make_card(image){ 
+#     console.log(image);
+# }
+
+def make_card(image):
+    #파이썬은 들여쓰기를 강제한다. 단 {}를 쓰지 않는다.
+    # : 얘가 들여쓰기를 명령한다. 
+    print(image)
+
+make_card('hello')
+
+
+#조건문
+def oddeven(num):
+    if num % 2 == 0: #if 다음에 ( ) 없음 : 들여쓰기로 구분
+        return True
+    else:
+        return False
+
+print(oddeven(10))
+
+
+
+#반복문
+fruits = ['사과', '배', '감', '귤']
+
+# for (let i=0;i<fruit.length;i++){
+#     console.log(fruits[i]);
+# }
+
+for fruit in fruits: # fruits 에서 하나씩 꺼내다가 fuit에 저장해서 쓴다.
+
+
+fruits = ['사과','배','배','감','수박','귤','딸기','사과','배','수박']
+
+count = 0 #사과 개수 저장하는 변수
+for fruit in fruits:
+    if fruit =='사과' :
+        count +=1
+
+
+
+people = [{'name': 'bob', 'age': 20}, 
+          {'name': 'carry', 'age': 38},
+          {'name': 'john', 'age': 7}]
+
+
+#모든 사람의 이름과 나이를 출력해보자
+def get_age(myname):
+    # my name에 들어온 이름의 나이를 출력하는 함수
+    for person in people :
+        if person == myname:
+            return person['age']
+    else :
+        return '해당하는 이름이 없습니다'
+
+print(get)
+
+get_age('bob') 
